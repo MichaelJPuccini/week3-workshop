87db9db44cc00246581932a1b9cb1a1f000c5e5c
MichaelJPuccini <michaeljpuccini@outlook.com>
2024-07-20 17:46:39 +1000

    Testing to see if I broke .gitignore

---
+++ server.js
@@ -0,0 +1,43 @@
+const PORT = 3000;
+
+var express = require('express'); //used for routing
+var formidable = require('formidable');
+
+var app = express();
+var http = require('http').Server(app); //used to provide http functionality
+
+app.use (express.static(__dirname + '/www')); //serve static files from the public directory
+app.use(express.urlencoded({extended: true})); // parse application/x-www-form-urlencoded
+app.use(express.json()) // parse application/json
+
+var loginAccounts = [
+    {email: 'admin@com.au', password: 'admin'},
+    {email: 'user@com.au', password: 'user'},
+    {email: 'guest@com.au', password: 'guest'}
+];
+
+// Week 3 Task 3
+app.post('/account', function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+    var customer = {};
+    customer.email = req.body.email;
+    customer.password = req.body.password;
+
+    // Check if the email and password match any of the accounts
+    let account = loginAccounts.find(account => account.email === customer.email && account.password === customer.password);
+    if (account) {
+        res.send ({valid: true});
+    } else {    
+        res.send ({valid: false});
+    }
+
+    // res.send(customer);
+});
+
+let server = http.listen (PORT, function () {
+    // let port = server.address().port;
+    console.log(`Server listening on: http://localhost:${PORT}/`);
+});
---
+++ www/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    console.log("Document Loaded...");
+    $("#loginform").submit(function(event) {
+        event.preventDefault();
+        post();
+
+    //     var username = $("#username").val();
+    //     var password = $("#password").val();
+    //     if (username === "admin" && password === "admin") {
+    //         alert("Login successful");
+    //     } else {
+    //         alert("Login failed");
+    //     }
+    // }
+    });
+});
+
+function post() {
+    console.log("Posting data...");
+    var formData = {
+        email : $("#email").val(),
+        password : $("#password").val()
+    };
+
+    // Post via AJAX
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/account",
+        data: JSON.stringify(formData),
+        dataType : "json",
+        success: function(customer) {
+            if (customer.valid == true) {
+                console.log("Successful Login...");
+                // Turn display on for success message
+                $("#successmsg").removeClass("hidden");
+                $("#successmsg").addClass("showmessage");
+                // Turn display off for error message
+                $("#errormsg").removeClass("showmessage");
+                $("#errormsg").addClass("hidden");
+            } else {
+                console.log("Unsuccessful Login...");
+                // Turn display on for error message
+                $("#errormsg").removeClass("hidden");
+                $("#errormsg").addClass("showmessage");
+                // Turn display off for success message
+                $("#successmsg").removeClass("showmessage");
+                $("#successmsg").addClass("hidden");
+            }
+        }
+    });
+}
